77faeecb8d9f713d13e1c83f571d451ac52ca509
RadhikaPPatel <70138493+RadhikaPPatel@users.noreply.github.com>
2021-07-22 12:31:11 +0530

    remove environment files

--- initialize_container.sh
+++ initialize_container.sh
@@ -3,7 +3,7 @@
 #Export the current commitID, branch and remote that the build was made from
 export GIT_FETCH_HEAD=`cat .git/FETCH_HEAD`
 
-#Read all environment variables as output by printenv and put them into an object stored in window.environment
+#Read all env variables as output by printenv and put them into an object stored in window.env
 RESULT='window.env = {'
 RESULT+='API_URL: "'$API_URL
 RESULT+='", OAUTH_CLIENT_ID: "'$OAUTH_CLIENT_ID
