76497db459391782fc42a43db68db1aaf17d827d
Radhika Patel <radhika.patel2206@gmail.com>
2024-01-11 17:05:04 +0530

    Users initial commit

--- src/pages/UserManagement/UserManagement.js
+++ src/pages/UserManagement/UserManagement.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Route, useHistory } from 'react-router-dom';
+import { Box, Tab, Tabs } from '@mui/material';
+import { routes } from '@routes/routesConstants';
+import Users from './Users/Users';
+import UserGroups from './UserGroups/UserGroups';
 
 const UserManagement = () => {
+  const history = useHistory();
+  const subNav = [
+    { label: 'Current users', value: 'current-users' },
+    { label: 'User groups', value: 'groups' },
+  ];
+  const viewPath = (
+    subNav.find((item) => location.pathname.endsWith(item.value)) || subNav[0]
+  ).value;
+  const [view, setView] = useState(viewPath);
+
+  // this will be triggered whenever the content switcher is clicked to change the view
+  useEffect(() => {
+    history.push(`/app/profile/users/${view || location.state}`);
+  }, [view]);
+
+  const viewTabClicked = (event, newView) => {
+    setView(newView);
+  };
+
   return (
-    <div>UserManagement</div>
-  )
-}
+    <Box mt={5} mb={3}>
+      <Box mb={3}>
+        <Tabs value={view} onChange={viewTabClicked}>
+          {subNav.map((itemProps, index) => (
+            <Tab {...itemProps} key={`tab${index}:${itemProps.value}`} />
+          ))}
+        </Tabs>
+      </Box>
+      <Route path={routes.CURRENT_USERS} component={Users} />
+      <Route path={routes.USER_GROUPS} component={UserGroups} />
+    </Box>
+  );
+};
 
 export default UserManagement;
--- src/pages/UserManagement/Users/Users.js
+++ src/pages/UserManagement/Users/Users.js
@@ -1,9 +1,156 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import _ from 'lodash';
+import { useQuery } from 'react-query';
+import {
+  VerifiedUser as ActivateIcon,
+  VerifiedUserOutlined as DeactivateIcon,
+} from '@mui/icons-material';
+import { IconButton, MenuItem, Select } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import DataTableWrapper from '@components/DataTableWrapper/DataTableWrapper';
+import { getUser } from '@context/User.context';
+import useAlert from '@hooks/useAlert';
+import { getAllOrganizationQuery } from 'react-query/queries/authUser/getAllOrganizationQuery';
+import { useEditCoreuserMutation } from 'react-query/mutations/coreuser/editCoreuserMutation';
+import { getCoregroupQuery } from 'react-query/queries/coregroup/getCoregroupQuery';
+import { getCoreuserQuery } from 'react-query/queries/coreuser/getCoreuserQuery';
+import { getGroupsFormattedRow, getUserFormattedRows, userColumns } from '@utils/constants';
+import { checkForAdmin } from '@utils/utilMethods';
+
+const useStyles = makeStyles((theme) => ({
+  iconDiv: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  iconButton: {
+    color: theme.palette.primary.dark,
+    '&:hover': {
+      backgroundColor: theme.palette.primary.light,
+    },
+  },
+}));
 
 const Users = () => {
+  const classes = useStyles();
+  const user = getUser();
+
+  const { displayAlert } = useAlert();
+  const [rows, setRows] = useState([]);
+  const [groups, setGroups] = useState([]);
+
+  const { data: coreuserData, isLoading: isLoadingCoreuser } = useQuery(
+    ['users'],
+    () => getCoreuserQuery(displayAlert),
+  );
+
+  const { data: coregroupData, isLoading: isLoadingCoregroup } = useQuery(
+    ['coregroup'],
+    () => getCoregroupQuery(displayAlert),
+  );
+
+  const { data: organizations, isLoading: isLoadingOrganizations } = useQuery(
+    ['organizations'],
+    () => getAllOrganizationQuery(displayAlert),
+  );
+
+  const { mutate: editUserMutation, isLoading: isEditingUser } = useEditCoreuserMutation(displayAlert);
+
+  useEffect(() => {
+    if (!_.isEmpty(coreuserData)) {
+      setRows(getUserFormattedRows(coreuserData));
+    }
+  }, [coreuserData]);
+
+  useEffect(() => {
+    if (!_.isEmpty(coregroupData) && !_.isEmpty(organizations)) {
+      setGroups(getGroupsFormattedRow(coregroupData, organizations));
+    }
+  }, [coregroupData, organizations]);
+
+  const activateDeactivateUser = (coreuser) => {
+    const editData = { id: coreuser.id, is_active: !coreuser.is_active };
+    editUserMutation(editData);
+  };
+
+  const updatePermissions = (e, coreuser) => {
+    const editData = { id: coreuser.id, core_groups: [e.target.value] };
+    editUserMutation(editData);
+  };
+
   return (
-    <div>Users</div>
-  )
-}
+    <div>
+      <DataTableWrapper
+        hideAddButton
+        centerLabel
+        filename="Users"
+        tableHeader="Users"
+        loading={isLoadingCoreuser || isLoadingCoregroup || isLoadingOrganizations || isEditingUser}
+        rows={rows || []}
+        columns={[
+          ...userColumns(),
+          {
+            name: 'Permissions',
+            options: {
+              sort: true,
+              sortThirdClickReset: true,
+              filter: true,
+              setCellHeaderProps: () => ({ style: { textAlign: 'center' } }),
+              customBodyRenderLite: (dataIndex) => {
+                const coreuser = rows[dataIndex];
+                return (
+                  <Select
+                    fullWidth
+                    id="coreuser-permissions"
+                    disabled={_.isEqual(user.id, coreuser.id) || !coreuser.is_active}
+                    value={coreuser.core_groups[0].id}
+                    onChange={(e) => updatePermissions(e, coreuser)}
+                  >
+                    {_.map(groups, (cg) => (
+                      <MenuItem key={`coregorup-${cg.id}`} value={cg.id}>
+                        {cg.display_permission_name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                );
+              },
+            },
+          },
+          {
+            name: 'Activate/Deactivate User',
+            options: {
+              filter: false,
+              sort: false,
+              empty: true,
+              setCellHeaderProps: () => ({ style: { textAlign: 'center' } }),
+              customBodyRenderLite: (dataIndex) => {
+                const coreuser = rows[dataIndex];
+                return (
+                  <div className={classes.iconDiv}>
+                    <IconButton
+                      disabled={_.isEqual(user.id, coreuser.id)}
+                      className={classes.iconButton}
+                      onClick={(e) => activateDeactivateUser(coreuser)}
+                    >
+                      {coreuser.is_active ? <ActivateIcon titleAccess="Deactivate" /> : <DeactivateIcon titleAccess="Activate" />}
+                    </IconButton>
+                  </div>
+                );
+              },
+            },
+          },
+        ]}
+        extraOptions={{
+          setRowProps: (row, dataIndex, rowIndex) => {
+            const coreuser = rows[dataIndex];
+            const style = { backgroundColor: '#BEBEBA' };
+            return !coreuser.is_active || _.isEqual(user.id, coreuser.id) ? { style } : {};
+          },
+        }}
+      />
+    </div>
+  );
+};
 
 export default Users;
