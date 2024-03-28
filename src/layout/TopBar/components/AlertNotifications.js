c31d54bbfa9f6d3bb62af7f47f36b959b278e57b
Radhika Patel <radhika.patel2206@gmail.com>
2024-03-28 14:16:03 +0530

    fix push notification for new alerts and already viewed

--- src/layout/TopBar/components/AlertNotifications.js
+++ src/layout/TopBar/components/AlertNotifications.js
@@ -68,11 +68,12 @@ const AlertNotifications = ({
 
   useEffect(() => {
     const notViewed = _.filter(notifications, { viewed: false });
-    console.log('not viewed', notViewed);
+    const oldNotifications = getViewedNotifications();
     let alerts = [];
     _.forEach(notViewed, (value) => {
-      alerts = [...alerts, ...value.alerts];
+      alerts = [...alerts, ..._.filter(value.alerts, (a) => !_.includes(oldNotifications, a.id))];
     });
+
     if (_.size(notViewed) > 0) {
       setHideAlertBadge(false);
       _.forEach(alerts, (value) => {
@@ -83,17 +84,16 @@ const AlertNotifications = ({
           subtitle: '',
           message: `${value.alert_message} | ${moment(value.create_date).fromNow()}`,
           onClick: (event) => {
-            const viewed = getViewedNotifications();
-            localStorage.setItem(
-              'viewedNotifications',
-              JSON.stringify(_.uniq([...viewed, value.id])),
-            );
+            const alertIndex = _.findIndex(notifications, { shipment_id: value.shipment_id });
+            handleAlertCountClick(alertIndex);
           },
         });
       });
     } else {
       setHideAlertBadge(true);
     }
+
+    alertSocketOnMessage();
   }, [notifications]);
 
   const reloadData = () => {
@@ -137,56 +137,71 @@ const AlertNotifications = ({
       }
     };
 
-    alertsSocket.current.onmessage = (message) => {
-      const msg = JSON.parse(message.data);
+    alertSocketOnMessage();
+  };
 
-      let alerts = [];
-      _.forEach(msg.alerts, (a) => {
-        const parameterType = _.toLower(_.split(a.alert_message, ' of ')[0]);
-        const parameterValue = _.split(_.split(a.alert_message, ' of ')[1], ' at ')[0];
-        let alertObj = { id: parameterType, color: 'green', title: `${_.capitalize(parameterType)} Excursion Recovered` };
-        if (_.isEqual(a.severity, 'error')) {
-          alertObj = { ...alertObj, color: theme.palette.error.main, title: `Maximum ${_.capitalize(parameterType)} Excursion` };
-        }
-        if (_.isEqual(a.severity, 'info')) {
-          alertObj = { ...alertObj, color: theme.palette.info.main, title: `Minimum ${_.capitalize(parameterType)} Excursion` };
-        }
-        alerts = [...alerts, { ...a, alertObj, parameterValue }];
-      });
+  const alertSocketOnMessage = () => {
+    if (alertsSocket.current) {
+      alertsSocket.current.onmessage = (message) => {
+        const msg = JSON.parse(message.data);
+        const viewedNotifications = getViewedNotifications();
 
-      const shipments = _.uniqBy(_.map(alerts, (a) => ({ id: a.shipment_id, name: a.shipment_name })), 'id');
-
-      let formattedAlerts = [];
-      _.forEach(shipments, (ship) => {
-        formattedAlerts = [
-          ...formattedAlerts,
-          {
-            shipment_id: ship.id,
-            shipment_name: ship.name,
-            alerts: _.orderBy(_.filter(alerts, { shipment_id: ship.id }), ['alert_time', 'id'], ['desc', 'asc']),
-            viewed: false,
-          },
-        ];
-      });
-
-      if (msg.command === 'fetch_alerts') {
-        setNotifications(formattedAlerts);
-      }
-      if (msg.command === 'new_alert') {
-        let finalNotifications = notifications;
-        _.forEach(formattedAlerts, (fa) => {
-          const alertIndex = _.findIndex(finalNotifications, (fn) => _.isEqual(fa.shipment_id, fn.shipment_id));
-          finalNotifications = _.mergeWith(finalNotifications[alertIndex], fa, customizer);
+        let alerts = [];
+        _.forEach(msg.alerts, (a) => {
+          const parameterType = _.toLower(_.split(a.alert_message, ' of ')[0]);
+          const parameterValue = _.split(_.split(a.alert_message, ' of ')[1], ' at ')[0];
+          let alertObj = { id: parameterType, color: 'green', title: `${_.capitalize(parameterType)} Excursion Recovered` };
+          if (_.isEqual(a.severity, 'error')) {
+            alertObj = { ...alertObj, color: theme.palette.error.main, title: `Maximum ${_.capitalize(parameterType)} Excursion` };
+          }
+          if (_.isEqual(a.severity, 'info')) {
+            alertObj = { ...alertObj, color: theme.palette.info.main, title: `Minimum ${_.capitalize(parameterType)} Excursion` };
+          }
+          alerts = [...alerts, { ...a, alertObj, parameterValue }];
         });
-        setNotifications(finalNotifications);
-        // invalidate queries to reload data
-        reloadData();
-      }
-      if (msg.command === 'reload_data') {
-        // invalidate queries to reload data
-        reloadData();
-      }
-    };
+
+        const shipments = _.uniqBy(_.map(alerts, (a) => ({ id: a.shipment_id, name: a.shipment_name })), 'id');
+
+        let formattedAlerts = [];
+        _.forEach(shipments, (ship) => {
+          const newAlerts = _.orderBy(_.filter(alerts, { shipment_id: ship.id }), ['alert_time', 'id'], ['desc', 'asc']);
+          let isViewed = true;
+          _.forEach(newAlerts, (a) => {
+            isViewed = isViewed && _.includes(viewedNotifications, a.id);
+          });
+
+          formattedAlerts = [
+            ...formattedAlerts,
+            {
+              shipment_id: ship.id,
+              shipment_name: ship.name,
+              alerts: newAlerts,
+              viewed: isViewed,
+            },
+          ];
+        });
+
+        if (msg.command === 'fetch_alerts') {
+          setNotifications(formattedAlerts);
+        }
+        if (msg.command === 'new_alert') {
+          let finalNotifications = notifications;
+          _.forEach(formattedAlerts, (fa) => {
+            const alertIndex = _.findIndex(finalNotifications, (fn) => _.isEqual(fa.shipment_id, fn.shipment_id));
+            const newNotification = _.mergeWith(finalNotifications[alertIndex], fa, customizer);
+            finalNotifications = [..._.slice(finalNotifications, 0, alertIndex), newNotification, ..._.slice(finalNotifications, alertIndex + 1)];
+          });
+          setNotifications(finalNotifications);
+
+          // invalidate queries to reload data
+          reloadData();
+        }
+        if (msg.command === 'reload_data') {
+          // invalidate queries to reload data
+          reloadData();
+        }
+      };
+    }
   };
 
   const closeAlertNotifications = () => {
@@ -196,7 +211,7 @@ const AlertNotifications = ({
 
   const getViewedNotifications = () => (
     localStorage.getItem('viewedNotifications')
-      ? JSON.parse(localStorage.getItem('viewedNotifications'))
+      ? _.sortBy(JSON.parse(localStorage.getItem('viewedNotifications')))
       : []
   );
 
