5be34d6b3983fdd350503f7f9ab4179e4368dd6c
TanmayJain92 <tanmay.jain@ajackus.com>
2024-03-28 20:49:36 +0530

    Fix issues

--- src/layout/TopBar/components/AlertNotifications.js
+++ src/layout/TopBar/components/AlertNotifications.js
@@ -77,18 +77,20 @@ const AlertNotifications = ({
     if (_.size(notViewed) > 0) {
       setHideAlertBadge(false);
       _.forEach(alerts, (value) => {
-        const viewedNoti = getViewedNotifications();
-        localStorage.setItem(
-          'viewedNotifications',
-          JSON.stringify(_.uniq([...viewedNoti, value.id])),
-        );
-
         addNotification({
           native: true,
           duration: window.env.hide_notification,
           title: appTitle,
           subtitle: '',
           message: `${value.alert_message} | ${moment(value.create_date).fromNow()}`,
+          onClick: (event) => {
+            setOpen(true);
+            const viewedNoti = getViewedNotifications();
+            localStorage.setItem(
+              'viewedNotifications',
+              JSON.stringify(_.uniq([...viewedNoti, value.id])),
+            );
+          },
         });
       });
     } else {
