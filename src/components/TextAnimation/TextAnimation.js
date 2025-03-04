6a33a9a77833973c361d48a791e113c2a9643df3
Radhika Patel <radhika.patel2206@gmail.com>
2025-03-04 11:15:56 +0530

    add close button for notification

--- src/components/TextAnimation/TextAnimation.js
+++ src/components/TextAnimation/TextAnimation.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { IconButton } from '@mui/material';
+import { Close as CloseIcon } from '@mui/icons-material';
 import './TextAnimation.css';
 
-const TextAnimation = () => (
-  <div className="textAnimationDiv">
-    <span className="textAnimationText">
-      Our website will be undergoing scheduled  maintenance from GMT 07:00 am Friday, 7th March until GMT 06:00 pm Sunday, 9th March.
-      During this period, you may experience temporary disruptions.
-      We apologize for any inconvenience and appreciate your patience as we work to improve our services.
-      Thank you for your understanding.
-      If you have any questions or need assistance, please contact us at support@transparentpath.com.
-    </span>
-  </div>
-);
+const TextAnimation = () => {
+  const [close, setClose] = useState(false);
+
+  return (
+    <div className={close ? 'textAnimationHidden' : 'textAnimationDiv'}>
+      <span className="textAnimationText">
+        Our website will be undergoing scheduled  maintenance from GMT 07:00 am Friday, 7th March until GMT 06:00 pm Sunday, 9th March.
+        During this period, you may experience temporary disruptions.
+        We apologize for any inconvenience and appreciate your patience as we work to improve our services.
+        Thank you for your understanding.
+        If you have any questions or need assistance, please contact us at support@transparentpath.com.
+      </span>
+      <IconButton onClick={() => setClose(true)}>
+        <CloseIcon />
+      </IconButton>
+    </div>
+  );
+};
 
 export default TextAnimation;
